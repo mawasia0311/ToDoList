f7c051b3413712abdcba0f9f0ceb05691136abe1
mawasia0311 <69418930+mawasia0311@users.noreply.github.com>
2022-09-28 06:21:12 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+// import PracticeComp from './component/PracticeComp';
+import TodoList from './TodoList/TodoList';
+
+
+function App() {
+
+  
+
+  return (
+    <div className="App">
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
